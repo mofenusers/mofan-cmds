7d100e0c9430300da02629cb988eb5e45aaaf9f6
夏国荣 <xhaguorong@szhtxx.com>
2023-03-02 11:09:11 +0800

    feat: init

---
+++ packages/chat/chattest.js
@@ -0,0 +1,31 @@
+import chatGPT from "chatgpt-io";
+import inquirer from "inquirer";
+
+const questions = [
+  {
+    type: "input",
+    name: "question",
+    message: "想问什么？",
+  },
+];
+(async () => {
+  let bot = new chatGPT(process.env.SESSION_TOKEN);
+  await bot.waitForReady();
+
+  const answer = async (question) => {
+    let response = await bot.ask(question);
+    console.log(response);
+    return response;
+  };
+  const questionInput = () => {
+    inquirer
+      .prompt(questions)
+      .then((res) => {
+        const { question } = res;
+        console.log("🚀 ~ file: chattest.js:27 ~ .then ~ question", question);
+        return answer(question);
+      })
+      .finally(questionInput);
+  };
+  questionInput();
+})();
